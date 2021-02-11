8b1f30952a7f661bdd3e1ed311c2f475803add7e
BrockMorris42611 <brock36morris@gmail.com>
2021-02-11 10:06:05 -0500

    added the error TextViews as well as checks for each box to make sure they do not contain the empty string. the error TextViews start out as invisible and are made visible upon error identification.

--- app/src/main/java/temple/edu/signupform/FormActivity.java
+++ app/src/main/java/temple/edu/signupform/FormActivity.java
@@ -7,9 +7,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class FormActivity extends AppCompatActivity {
-
 
     EditText email_tb; //define the text fields for the info input
     EditText name_tb;
@@ -17,7 +17,18 @@ public class FormActivity extends AppCompatActivity {
     EditText passConf_tb;
 
     Button send; //define button to confirm all info
+
     UserLoginInfo arriving_user; //use about which we store info
+
+    TextView email_background;
+    TextView name_background;
+    TextView pass_background;
+    TextView passConf_background;
+
+    TextView email_err_mssg;
+    TextView name_err_mssg;
+    TextView pass_err_mssg;
+    TextView passConf_err_mssg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,18 @@ public class FormActivity extends AppCompatActivity {
         passConf_tb = findViewById(R.id.passwordConf_entry);
         send = findViewById(R.id.send_info); //set up the button
 
+        email_background = findViewById(R.id.email_view); //these are all the views behind the sections and boxes for info entry
+        name_background =  findViewById(R.id.name_view);
+        pass_background = findViewById(R.id.password_view);
+        passConf_background = findViewById(R.id.confirm_view);
+
+        email_err_mssg = findViewById(R.id.email_err_view);
+        name_err_mssg = findViewById(R.id.name_err_view);
+        pass_err_mssg = findViewById(R.id.pass_err_view);
+        passConf_err_mssg = findViewById(R.id.conf_err_view);
+
+
+
         send.setOnClickListener(new View.OnClickListener() { //get the button ready to listen for click
             @Override
             public void onClick(View v) {
@@ -38,18 +61,30 @@ public class FormActivity extends AppCompatActivity {
                         name_tb.getText().toString(),
                         pass_tb.getText().toString(),
                         passConf_tb.getText().toString());
+                check_input(arriving_user);
                 Log.v("email> ", arriving_user.getEmail()); //print it all to logs to make sure
                 Log.v("name> ", arriving_user.getName());
                 Log.v("password> ",arriving_user.getPassword());
-                Log.v("confirmed pasword> ", arriving_user.getPasswordConf());
+                Log.v("confirmed password> ", arriving_user.getPasswordConf());
             }
         });
-
-
     }
+    public void check_input(UserLoginInfo new_info){ //make sure all our input is valid
 
+        if(new_info.getEmail().equals(""))              //these are all checking for empty string
+            email_err_mssg.setVisibility(View.VISIBLE);
+        if(new_info.getName().equals(""))
+            name_err_mssg.setVisibility(View.VISIBLE);
+        if(new_info.getPassword().equals(""))
+            pass_err_mssg.setVisibility(View.VISIBLE);
+        if(new_info.getPasswordConf().equals(""))
+            passConf_err_mssg.setVisibility(View.VISIBLE);
 
-
+        if(!new_info.getPassword().equals(new_info.getPasswordConf())){
+            passConf_err_mssg.setText(R.string.match_err);
+            passConf_err_mssg.setVisibility(View.VISIBLE);
+        }
+    }
 }
 class UserLoginInfo {
 
@@ -58,7 +93,7 @@ class UserLoginInfo {
     private String password;
     private String passwordConf;
 
-    public UserLoginInfo(String name, String email, String password, String passwordConf){
+    public UserLoginInfo(String email, String name, String password, String passwordConf){
         this.name = name;
         this.email = email;
         this.password = password;
